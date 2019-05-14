157250243cd8e7e02d665377883e4f2648d70dd3
Alfreda Tsai <alfredatsai@gmail.com>
2019-05-14 16:17:18 -0600

    all basic objectives

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,47 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
   end
 
   def show
+    @people = Person.find(params[:id])
   end
 
   def new
+    @people = Person.new
   end
+
+  def edit
+    @people = Person.find(params[:id])
+  end
+
+  def update
+    @people = Person.find(params[:id])
+    
+    if @people.update(people_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end
+
+  def create
+    @people = Person.new(people_params)
+
+    if @people.save
+      redirect_to people_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def people_params
+      params.require(:person).permit(:first_name, :last_name, :age, :hair_color, :eye_color, :gender, :alive)
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,21 @@
 class Person < ApplicationRecord
-end
+
+  # Class Method
+  def self.by_first_name
+    order(:first_name)
+  end
+
+  # Instance Method
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+
+  # <% @people.each do |person| %>
+  #   <h1> <%= person.first_name %> <%= person.last_name %> </h1>
+  #   <h1> <%= person.full_name %> </h1>
+  # <% end %>
+
+  end
+
+ 
